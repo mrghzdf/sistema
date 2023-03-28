95cf44fb9f196e11e7ec749739f39594e8d13136
djalmabsb <djalma.df1@gmail.com>
2023-03-27 22:40:56 -0300

    Arquivos de produção

--- view/config.php
+++ view/config.php
@@ -10,19 +10,19 @@ define( 'UP_ABSPATH', str_replace('adm', '', APPRAIZ) . 'img/_uploads' );
 // Charset da conexão PDO
 define( 'DB_CHARSET', 'utf8' );
 
-//define( 'HOME_URI', 'http://opontosite.com.br/sistema/view/' ); // PROD
-//define( 'HOME_URI_SITE', 'http://opontosite.com.br/sistema/' ); // PROD
-//define( 'URLFOTO', 'http://opontosite.com.br/sistema/adm/public/img/' ); // PROD
-//define( 'HOSTNAME', 'localhost' ); // PROD
-//define( 'DB_USER', 'root' ); // PROD
-//define( 'DB_PASSWORD', 'teste' ); // PROD
+define( 'HOME_URI', 'http://www.odontox.com.br/sistema/view/' ); // PROD
+define( 'HOME_URI_SITE', 'http://www.odontox.com.br/sistema/' ); // PROD
+define( 'URLFOTO', 'http://www.odontox.com.br/sistema/adm/assets/public/images/fotos/' ); // PROD
+define( 'HOSTNAME', 'fbb_sistema.mysql.dbaas.com.br' ); // PROD
+define( 'DB_USER', 'fbb_sistema' ); // PROD
+define( 'DB_PASSWORD', 'Fbb@1467' ); // PROD
 
-define( 'HOME_URI', 'http://localhost/opontosite/sistema/view/' ); // LOCAL
-define( 'HOME_URI_SITE', 'http://localhost/opontosite/' ); // LOCAL
-define( 'URLFOTO', 'http://localhost/opontosite/sistema/public/img/' ); // LOCAL
-define( 'HOSTNAME', 'localhost' ); // LOCAL
-define( 'DB_USER', 'root' ); // LOCAL
-define( 'DB_PASSWORD', '' ); // LOCAL
+//define( 'HOME_URI', 'http://localhost/opontosite/sistema/view/' ); // LOCAL
+//define( 'HOME_URI_SITE', 'http://localhost/opontosite/' ); // LOCAL
+//define( 'URLFOTO', 'http://localhost/opontosite/sistema/public/assets/images/fotos' ); // LOCAL
+//define( 'HOSTNAME', 'localhost' ); // LOCAL
+//define( 'DB_USER', 'root' ); // LOCAL
+//define( 'DB_PASSWORD', '' ); // LOCAL
 
 // Nome do DB
 define( 'DB_NAME', 'fbb_sistema' );
